2246a8de37b532668f6cdc174b037e404591588c
“liudaOz” <liuda.oz2023js@gmail.com>
2024-02-26 20:27:53 +0200

    step 1

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function sum(a, b) {
 }
 
 console.log(sum(2, 4));
+
+
+class MyMath {
+    static sum = (a, b) => a+b;
+    static sub = (a, b) => a-b;
+    static div = (a, b) => a/b;
+    static multy = (a, b) => a*b;
+}
+
+
+class Component {
+    render() {
+        return 'Test Component'
+    }
+}
+
+const sum2n2 = MyMath.sum(2, 2);
+console.log(sum2n2);
+const multy3n5 = MyMath.multy(2, 2);
+console.log(multy3n5);
